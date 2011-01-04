c5eeae156afdd74f577c71b3e7bac94d2a9405ef
Tom Payne <twpayne@gmail.com>
2011-01-04 16:10:42 +0100

    Factor out Waypoint class and use MVCObjects

--- templates/waypointviewer.js
+++ templates/waypointviewer.js
@@ -1,3 +1,65 @@
+function Waypoint(options) {
+
+	this.set('id', options.id);
+	this.set('position', options.position);
+	this.set('elevation', options.elevation);
+	this.set('description', options.description);
+	this.set('radius', options.radius);
+	this.set('color', options.color);
+	this.set('map', options.map);
+
+	var richMarkerContent = $('#richMarker').clone().attr({id: null}).show();
+	richMarkerContent.css('background-color', '#' + options.color);
+	var label = this.get('id');
+	var match = /^([A-Z][0-9]{2})([0-9]{3})$/.exec(this.get('id'));
+	if (match && 10 * (match[2] - 1) <= this.get('elevation') && this.get('elevation') <= 10 * (match[2] + 1)) {
+		label = match[1];
+	}
+	$('#label', richMarkerContent).html(label);
+	var marker = new RichMarker({
+		anchor: RichMarkerPosition.MIDDLE,
+		content: richMarkerContent.get(0),
+		flat: true,
+		map: this.get('map')
+	});
+	marker.bindTo('position', this);
+	marker.bindTo('map', this);
+
+	var circle = new google.maps.Circle({
+		clickable: false,
+		fillOpacity: 0.25,
+		strokeOpacity: 1,
+		strokeWeight: 1
+	});
+	circle.bindTo('center', this, 'position');
+	circle.bindTo('fillColor', this, 'color');
+	circle.bindTo('radius', this);
+	circle.bindTo('strokeColor', this, 'color');
+	circle.bindTo('map', this);
+
+	var infoWindowContent = $('#infoWindow').clone().attr({id: null}).show();
+	$('#id', infoWindowContent).html(this.get('id'));
+	$('#lat', infoWindowContent).html(this.get('position').lat());
+	$('#lng', infoWindowContent).html(this.get('position').lng());
+	$('#elevation', infoWindowContent).html(this.get('elevation'));
+	$('#description', infoWindowContent).html(this.get('description'));
+	$('#radius', infoWindowContent).html(this.get('radius'));
+	$('#color', infoWindowContent).html(this.get('color'));
+	var infoWindow = new google.maps.InfoWindow({content: infoWindowContent.get(0)});
+
+	google.maps.event.addListener(marker, 'click', function () {
+		infoWindow.open(map, marker);
+	});
+
+	google.maps.event.addListener(marker, 'dblclick', function () {
+		map.panTo(position);
+		map.setZoom(15);
+	});
+
+}
+
+Waypoint.prototype = new google.maps.MVCObject();
+
 $(document).ready(function(){
 	$.getJSON("wpt2json.json?url={{ url|addslashes }}", function(geojson) {
 		var map = new google.maps.Map($('#map').get(0), {
@@ -13,66 +75,28 @@ $(document).ready(function(){
 			map.fitBounds(bounds);
 		});
 		$.each(geojson.features, function(i, feature) {
-			var color = 'ffff00';
+			var options = {
+				color: 'ffff00',
+				description: '',
+				elevation: 0,
+				id: feature.properties.id,
+				map: map,
+				position: new google.maps.LatLng(
+					feature.geometry.coordinates[1],
+					feature.geometry.coordinates[0]
+				),
+				radius: 400
+			};
 			if (feature.properties.hasOwnProperty('color')) {
-				color = feature.properties.color;
+				options.color = feature.properties.color;
 			}
-			var elevation = 0;
 			if (feature.geometry.coordinates.length > 2) {
-				elevation = feature.geometry.coordinates[2];
+				options.elevation = feature.geometry.coordinates[2];
 			}
-			var radius = 400;
 			if (feature.properties.hasOwnProperty('radius')) {
-				radius = feature.properties.radius;
+				options.radius = feature.properties.radius;
 			}
-			var position = new google.maps.LatLng(
-				feature.geometry.coordinates[1],
-				feature.geometry.coordinates[0]
-			);
-			var richMarkerContent = $('#richMarker').clone().attr({id: null}).show();
-			richMarkerContent.css('background-color', '#' + color);
-			var label = feature.properties.id;
-			var match = /^([A-Z][0-9]{2})([0-9]{3})$/.exec(feature.properties.id);
-			if (match && 10 * (match[2] - 1) <= elevation && elevation <= 10 * (match[2] + 1)) {
-				label = match[1];
-			}
-			$('#label', richMarkerContent).html(label);
-			var marker = new RichMarker({
-				anchor: RichMarkerPosition.MIDDLE,
-				content: richMarkerContent.get(0),
-				flat: true,
-				map: map,
-				position: position
-			});
-			var infoWindowContent = $('#infoWindow').clone().attr({id: null}).show();
-			$('#id', infoWindowContent).html(feature.properties.id);
-			$('#lat', infoWindowContent).html(feature.geometry.coordinates[1]);
-			$('#lng', infoWindowContent).html(feature.geometry.coordinates[0]);
-			$('#elevation', infoWindowContent).html(elevation);
-			if (feature.properties.hasOwnProperty('description')) {
-				$('#description', infoWindowContent).html(feature.properties.description);
-			}
-			$('#radius', infoWindowContent).html(radius);
-			$('#color', infoWindowContent).html(color);
-			var circle = new google.maps.Circle({
-				center: position,
-				clickable: false,
-				fillColor: color,
-				fillOpacity: 0.25,
-				map: map,
-				radius: radius,
-				strokeColor: color,
-				strokeOpacity: 1,
-				strokeWeight: 1
-			});
-			var infoWindow = new google.maps.InfoWindow({content: infoWindowContent.get(0)});
-			google.maps.event.addListener(marker, 'click', function () {
-				infoWindow.open(map, marker);
-			});
-			google.maps.event.addListener(marker, 'dblclick', function () {
-				map.panTo(position);
-				map.setZoom(15);
-			});
+			var waypoint = new Waypoint(options);
 		});
 	});
 });
