580bf59e0b0d3895415d211b5810739c5423603c
Tom Payne <twpayne@gmail.com>
2011-01-05 15:54:36 +0100

    Add CompeGPS waypoint format support

--- waypointviewer.py
+++ waypointviewer.py
@@ -60,7 +60,31 @@ class Wpt2json(webapp.RequestHandler):
             feature_collection_properties['status_code'] = response.status_code
         lines = response.content.splitlines()
         features = []
-        if len(lines) >= 4 and re.match(r'\AOziExplorer\s+Waypoint\s+File\s+Version\s+\d+\.\d+\s*\Z', lines[0]) and re.match(r'\AWGS\s+84\s*\Z', lines[1]):
+        if len(lines) >= 2 and re.match(r'\AG\s+WGS\s+84\s*\Z', lines[0]) and re.match(r'\AU\s+1\s*\Z', lines[1]):
+            for line in lines[2:]:
+                match = re.match(r'\AW\s+(\S+)\s+A\s+(\d+\.\d+).*([NS])\s+(\d+\.\d+).*([EW])\s+\d{2}-(?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)-\d{2}\s+\d{2}:\d{2}:\d{2}\s+(-?\d+(?:\.\d+))(?:\s+(.*))?\Z', line)
+                if match:
+                    coordinates = [float(match.group(4)), float(match.group(2)), float(match.group(6))]
+                    if match.group(5) == 'S':
+                        coordinates[0] = -coordinates[0]
+                    if match.group(3) == 'W':
+                        coordinates[1] = -coordinates[1]
+                    feature_properties = {}
+                    feature_properties['id'] = match.group(1)
+                    feature_properties['description'] = match.group(7)
+                    feature = {'type': 'Feature', 'geometry': {'type': 'Point', 'coordinates': coordinates}, 'properties': feature_properties}
+                    features.append(feature)
+                    continue
+                match = re.match('\Aw\s+Waypoint,\d+,-?\d+(?:\.\d+)?,\d+,(\d+),\d+,\d+,[^,]*,(-?\d+(?:\.\d+)?)', line)
+                if match and len(features) > 0:
+                    feature_properties = features[-1]['properties']
+                    color = int(match.group(1))
+                    feature_properties['color'] = '%02x%02x%02x' % (color & 0xff, (color >> 8) & 0xff, (color >> 16) & 0xff)
+                    radius = float(match.group(2))
+                    if radius > 0.0:
+                        feature_properties['radius'] = radius
+                    continue
+        elif len(lines) >= 4 and re.match(r'\AOziExplorer\s+Waypoint\s+File\s+Version\s+\d+\.\d+\s*\Z', lines[0]) and re.match(r'\AWGS\s+84\s*\Z', lines[1]):
             for line in lines[4:]:
                 fields = re.split(r'\s*,\s*', line)
                 coordinates = [float(fields[3]), float(fields[2]), 0.3048 * float(fields[14])]
